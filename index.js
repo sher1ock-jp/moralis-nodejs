76868a0eea9aab1d3d07be40161c2c61c9ba4440
takayuki ono <85065480+takayukiono1001@users.noreply.github.com>
2023-06-01 13:55:43 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { EvmChain } = require("@moralisweb3/common-evm-utils");
 const app = express();
 const port = 3000;
 
-const MORALIS_API_KEY = "HPww5wkdwitpN1EU8voNzs2NJHuuMwhD5HXtnRf2rRChCdwEqDnaBtTUEs5tdv8g";
+const MORALIS_API_KEY = "XXX";
 const address = "0x1CA2E50Ba6E3E62f7b108BD32A6BD9e71a82cD77";
 const chain = EvmChain.POLYGON;
 
