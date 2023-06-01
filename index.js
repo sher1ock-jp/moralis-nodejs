36acfefad26031c84a1fc2b14c97ea475a71df1d
takayukiono1001 <takaaaaadayo@gmail.com>
2023-06-01 10:57:21 +0900

    all done

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const Moralis = require("moralis").default;
+const { EvmChain } = require("@moralisweb3/common-evm-utils");
+
+const app = express();
+const port = 3000;
+
+const MORALIS_API_KEY = "HPww5wkdwitpN1EU8voNzs2NJHuuMwhD5HXtnRf2rRChCdwEqDnaBtTUEs5tdv8g";
+const address = "0x1CA2E50Ba6E3E62f7b108BD32A6BD9e71a82cD77";
+const chain = EvmChain.POLYGON;
+
+async function getDemoData() {
+  // Get native balance
+  const nativeBalance = await Moralis.EvmApi.balance.getNativeBalance({
+    address,
+    chain,
+  });
+
+  // Format the native balance formatted in ether via the .ether getter
+  const native = nativeBalance.result.balance.ether;
+
+  // Get token balances
+  const tokenBalances = await Moralis.EvmApi.token.getWalletTokenBalances({
+    address,
+    chain,
+  });
+
+  // Format the balances to a readable output with the .display() method
+  const tokens = tokenBalances.result.map((token) => token.display());
+
+  // Get the nfts
+  const nftsBalances = await Moralis.EvmApi.nft.getWalletNFTs({
+    address,
+    chain,
+    limit: 10,
+  });
+
+  // Format the output to return name, amount and metadata
+  const nfts = nftsBalances.result.map((nft) => ({
+    name: nft.result.name,
+    amount: nft.result.amount,
+    metadata: nft.result.metadata,
+  }));
+
+  return { native, tokens, nfts };
+}
+
+app.get("/demo", async (req, res) => {
+  try {
+    // Get and return the crypto data
+    const data = await getDemoData();
+    res.status(200);
+    res.json(data);
+  } catch (error) {
+    // Handle errors
+    console.error(error);
+    res.status(500);
+    res.json({ error: error.message });
+  }
+});
+
+const startServer = async () => {
+  await Moralis.start({
+    apiKey: MORALIS_API_KEY,
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+};
+
+startServer();
